d9c2672002fa4034cbaf387c34e795bbcd46770f
mosiahazuaje2019 <mosiahazuaje2010@gmail.com>
2023-12-10 22:41:48 -0500

    arreglos de upload excel

--- app/Imports/BookingImport.php
+++ app/Imports/BookingImport.php
@@ -5,6 +5,7 @@ namespace App\Imports;
 use App\Models\Booking;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Carbon\Carbon;
 
 class BookingImport implements ToModel, WithHeadingRow
 {
@@ -15,9 +16,19 @@ class BookingImport implements ToModel, WithHeadingRow
      */
     public function model(array $row)
     {
+        $dateOnDays = $row['date'];
+        $dateBase   = Carbon::createFromDate(1900, 1, 1);
+        $date       = $dateBase->addDays($dateOnDays -2);
+        
         return new Booking([
-            'pax' => $row['pax'],
-            'service' => $row['service']
+            'pax'         => $row['pax'],
+            'service'     => $row['service'],
+            'client_name' => $row['client_name'],
+            'hotel'       => $row['hotel'],
+            'flight'      => $row['flight'],
+            'date'        => $date,
+            'time'        => $row['time'],
+            'comments'    => $row['comments']
         ]);
     }
 }
